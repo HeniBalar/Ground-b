46b74b5d13b2469772bd9bd65112a34dd4dc1617
Femil555 <161271077+Femil555@users.noreply.github.com>
2024-05-23 14:30:54 +0530

    update forgot password

--- controllers/signin/forgetPasswordController.js
+++ controllers/signin/forgetPasswordController.js
@@ -1,17 +1,34 @@
 const User = require("../../models/User");
 const bcrypt=require('bcryptjs')
 
-exports.forgotPassword = async(req,res) => {
+// exports.forgotPassword = async(req,res) => {
+//     try {
+//         const u = await User.getUserInfo(req.body.email);
+//         const isMatch = await bcrypt.compare(req.body.password, u.password);
+//         if (isMatch) {
+//             throw new Error("Old password & Current password can't be same")
+//         }
+//         const user = await User.findOneAndUpdate({_id: u._id},{password: await bcrypt.hash(req.body.password, 8)},{returnDocument: 'after'})
+//         await user.save()
+//         res.status(200).send({message: "Your password was successfully reset"})
+//     } catch (error) {
+//         res.send({error: error.message})
+//     }
+// }
+
+exports.forgotPassword = async (req, res) => {
     try {
-        const u = await User.getUserInfo(req.body.email);
-        const isMatch = await bcrypt.compare(req.body.password, u.password);
-        if (isMatch) {
-            throw new Error("Old password & Current password can't be same")
+        const { email } = req.body;
+        const u = await User.findOne({ email })
+        // console.log("finduser===>>>", u)
+        if(!u){
+            return res.status(404).send({message: "User not found with this email address!"})
         }
-        const user = await User.findOneAndUpdate({_id: u._id},{password: await bcrypt.hash(req.body.password, 8)},{returnDocument: 'after'})
-        await user.save()
-        res.status(200).send({message: "Your password was successfully reset"})
+        const user = await User.findByIdAndUpdate({ _id: u._id }, { $set: { password: await bcrypt.hash(req.body.password, 8) } }, { returnDocument: 'after' })
+        user.save()
+        return res.status(200).send({ message: "Update password successfully." })
     } catch (error) {
-        res.send({error: error.message})
+        console.error({ error: error })
+        return res.status(500).send({ error: error.message })
     }
 }
