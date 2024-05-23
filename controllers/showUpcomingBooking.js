cbfe854279d7368555632561936852ec0391a9cd
Femil555 <161271077+Femil555@users.noreply.github.com>
2024-05-23 14:32:28 +0530

    create upcoming booking controller

---
+++ controllers/showUpcomingBooking.js
@@ -0,0 +1,25 @@
+const Booking = require("../models/Booking")
+const User = require("../models/User")
+
+exports.showUpcomingBooking = async(req, res) => {
+    try{
+        const { email } = req.body
+        const user = await User.findOne({ email: email }); // console.log('user.....', user)
+        let bookings;
+        if(user.usertype === "owner"){
+            bookings = await Booking.find({ownerid: user._id})
+        }else{
+            bookings = await Booking.find({bookedby: user._id})
+        }
+
+        bookings.forEach(booking => {
+            if(booking.status == "Upcoming") return res.status(200).send({ bookings })
+                else return res.status(404).send({ message: "Upcoming bookings are not found." })
+        })
+    }catch(error){
+        console.error({ error: error.message })
+        res.send({ error: error.message })
+    }
+}
+
+//create upcoming pending complate booking API
