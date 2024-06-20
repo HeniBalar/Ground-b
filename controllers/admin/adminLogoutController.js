97f4c1269626aeb6b32d25c3c546287ff41c4d71
appnwebdeveloper <85012492+appnwebdeveloper@users.noreply.github.com>
2024-06-20 00:22:29 -0600

    set admin logout

--- controllers/admin/adminLogoutController.js
+++ controllers/admin/adminLogoutController.js
@@ -15,7 +15,7 @@ const Admin = require('../../models/Admin')
 exports.logoutAll = async(req,res) =>{
     try{
         await Admin.findByIdAndUpdate({_id: req.admin._id},{tokens: [], lasttimeloggedout: new Date()})
-        res.sendStatus(200)
+        res.status(200).send({ msg: "Admin Logout Successfully"})  
     }catch(error){
         res.send({error: error.message})
     }   
